d10a2ef6716183ad99639f2491a08b2d59fb38f3
Oleg Evseev <ev.mipt@gmail.com>
2020-01-14 21:34:47 +0300

    fix NMTstartup handling if device is NOT the NMT Master and now OD_NMTStartup (0x1F80 OD) can be optional
    issue #149

--- CANopen.c
+++ CANopen.c
@@ -824,7 +824,11 @@ CO_NMT_reset_cmd_t CO_process(
             CO->NMT,
             timeDifference_ms,
             OD_producerHeartbeatTime,
+#if (CO_NO_NMT_MASTER == 1) && defined(OD_NMTStartup)
             OD_NMTStartup,
+#else
+			0U,
+#endif
             OD_errorRegister,
             OD_errorBehavior,
             timerNext_ms);
