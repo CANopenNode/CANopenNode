c0c01e1be5b253305959fc79ae9587102edcea2c
Janez <janez.paternoster@siol.net>
2020-12-07 21:03:41 +0100

    move timeOfDay_t union to CO_TIME.h

--- stack/CO_TIME.h
+++ stack/CO_TIME.h
@@ -73,6 +73,21 @@ extern "C" {
 
 #define TIME_MSG_LENGTH 6U
 
+#ifndef timeOfDay_t
+typedef union {
+    unsigned long long ullValue;
+    struct {
+        unsigned long ms:28;
+        unsigned reserved:4;
+        unsigned days:16;
+        unsigned reserved2:16;
+    };
+}timeOfDay_t;
+#endif
+
+typedef timeOfDay_t TIME_OF_DAY;
+typedef timeOfDay_t TIME_DIFFERENCE;
+
 /**
  * TIME producer and consumer object.
  */
