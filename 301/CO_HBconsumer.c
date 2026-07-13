dc3cf84952beafec5af4d5748f0f14bf4eb90819
Frab012 <f.rabold@protronic-gmbh.de>
2026-07-13 12:24:41 +0200

    Fix HB state query for consecutive boot messages

--- 301/CO_HBconsumer.c
+++ 301/CO_HBconsumer.c
@@ -327,11 +327,9 @@ CO_HBconsumer_process(CO_HBconsumer_t* HBcons, bool_t NMTisPreOrOperational, uin
                                                                monitoredNode->functSignalObjectRemoteReset);
                     }
 #endif
-                    if (monitoredNode->HBstate == CO_HBconsumer_ACTIVE) {
+                    if (monitoredNode->HBstate != CO_HBconsumer_UNKOWN) {
                         CO_errorReport(HBcons->em, CO_EM_HB_CONSUMER_REMOTE_RESET, CO_EMC_HEARTBEAT, i);
                         monitoredNode->HBstate = CO_HBconsumer_RESTART;
-                    } else {
-                        monitoredNode->HBstate = CO_HBconsumer_UNKNOWN;
                     }
 
                 } else {
